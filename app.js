a8f9d14884ec26dd10d3ad2bffb453b01323529c
CharlesChinedum <charleschinedum2@gmail.com>
2023-06-28 23:12:54 +0100

    added general route for GET, POST, DELETE and added GET, POST, PUT for specific article routes

--- app.js
+++ app.js
@@ -23,19 +23,84 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-const findArticles = async () => {
-  try {
-    const articles = await Article.find();
-    console.log(articles.map((article) => article.title));
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const findArticles = async () => {
+//   try {
+//     const articles = await Article.find();
+//     return articles;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-  findArticles();
-});
+//Request targeting all articles
+
+app
+  .route("/articles")
+
+  .get(async (req, res) => {
+    try {
+      const articles = await Article.find();
+      res.send(articles);
+    } catch (error) {
+      res.send(error);
+    }
+  })
+
+  .post(async (req, res) => {
+    try {
+      const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content,
+      });
+      await newArticle.save();
+      res.send("Successfully added a new article");
+      console.log(newArticle);
+    } catch (error) {
+      res.send(error);
+    }
+  })
+
+  .delete(async (req, res) => {
+    try {
+      await Article.deleteMany();
+      res.send("Successfully deleted all articles");
+    } catch (error) {
+      res.send(error);
+    }
+  });
+
+//Requests targetting a specific Article
+
+app
+  .route("/articles/:articleTitle")
+
+  .get((req, res) => {
+    async function findArticle() {
+      const article = await Article.findOne({ title: req.params.articleTitle });
+      if (article) {
+        return article;
+      } else {
+        return "not found";
+      }
+    }
+
+    findArticle().then((result) => {
+      res.send(result);
+    });
+  })
+
+  .put(async (req, res) => {
+    try {
+      await Article.findOneAndUpdate(
+        { title: req.params.articleTitle },
+        { title: req.body.title, content: req.body.content },
+        { overwrite: true }
+      );
+      res.send("Successfully updated article");
+    } catch (err) {
+      res.status(500).send("Error updating article");
+    }
+  });
 
 app.listen(3000, () => {
   console.log("speak chief, I'm listening on port 3000");
